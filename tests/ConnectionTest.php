774075ad559e1584702e2d332628c2147f7ab3d6
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2022-04-14 01:25:49 +0300

    Initial testing files

---
+++ tests/ConnectionTest.php
@@ -0,0 +1,13 @@
+<?php
+use PHPUnit\Framework\TestCase;
+ 
+/**
+ * Description of ConnectionTest
+ *
+ * @author Ibrahim
+ */
+class ConnectionTest extends TestCase{
+    public function test00() {
+        $this->assertTrue(true);
+    }
+}
