026c582d728d2f36759672da3c297c8dd6937e1b
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2022-04-14 01:48:27 +0300

    Update

---
+++ testApp/ini/InitMiddleware.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace testApp\ini;
+
+class InitMiddleware {
+    /**
+     * Register middleware which are created outside the folder 'app/middleware'.
+     * 
+     * @since 1.0
+     */
+    public static function init() {
+        
+    }
+}
---
+++ testApp/ini/InitPrivileges.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace testApp\ini;
+
+class InitPrivileges {
+    /**
+     * Initialize user groups and privileges.
+     * 
+     * @since 1.0
+     */
+    public static function init() {
+        
+    }
+}
